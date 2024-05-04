9c309c58abca9cad7b18a51181d911e5bd81fd6b
João Pedro <joaopsn1@outlook.com.br>
2024-05-04 17:45:49 -0300

    Update MinhaClasse.java

--- bootcamp-santander/intellij/java-anatomia-classes/src/MinhaClasse.java
+++ bootcamp-santander/intellij/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,28 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Salve");
+        System.out.println("Olá, Mundo!");
+
+        int ano = 2024; // número inteiro
+        System.out.println(ano);
+
+        String BR = "Brasil"; // palavras
+        System.out.println(BR);
+
+        String meuNome = "João Pedro";
+        System.out.println(meuNome);
+
+        int anoFabricacao = 2022;
+        System.out.println(anoFabricacao);
+
+        boolean verdadeira = true; // variável booleana
+        System.out.println(verdadeira);
+
+        String primeiroNome = "João Pedro";
+        String segundoNome = "Souza Nunes";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) { // método
+        return "Resultado do método é " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
