457e3007c46a1d7078948fdc8167c19cbcf6c8d7
João Pedro <joaopsn1@outlook.com.br>
2024-05-07 21:58:24 -0300

    Update TiposVariaveis.java

--- bootcamp-santander/intellij/java-tipos-variaveis/src/TiposVariaveis.java
+++ bootcamp-santander/intellij/java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
+
+        double salarioMinimo = 2.500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Cast
+
+        final double VALOR_DE_PI = 3.14; // Precisa do "final" para não permitir alterar a variável 
     }
 }
