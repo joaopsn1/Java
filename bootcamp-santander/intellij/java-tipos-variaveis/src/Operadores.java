1668d0e4a82f8cddbe2845dc1c0f9c669e0abcaa
João Pedro <joaopsn1@outlook.com.br>
2024-05-10 10:53:08 -0300

    Create Operadores.java

---
+++ bootcamp-santander/intellij/java-tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repetição
+        numero++; // mesma coisa que numero = numero +1
+
+        System.out.println(numero);
+
+        boolean variavel = false;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
