f2b822c060b721cc49f33451075a8184b165b5b2
João Pedro <joaopsn1@outlook.com.br>
2024-07-10 16:03:39 -0300

    .

--- bootcamp-santander/intellij/desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
+++ bootcamp-santander/intellij/desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,29 +1,16 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
     //Atributos
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
     //Métodos
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -37,8 +24,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
